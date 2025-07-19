c0e99016fe370f589ae044afcbc94a1761244bd5
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-19 18:14:17 -0300

    🔵 Refactor: adição de dependência (Auth Module) ao módulo principal da aplicação (App Module)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [AuthModule] /* Importação de outros módulos */,
+  controllers: [AppController] /* Controllers pertencentes a este módulo */,
+  providers: [AppService] /* Providers (serviços) usados neste módulo */,
+  exports: [] /* Torna estes serviços visíveis para outros módulos */,
 })
 export class AppModule {}
