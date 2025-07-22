825da3ca65d3be298ec6fb7c1b844a0c7f2289bc
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-22 08:50:48 -0300

    🔵 Refactor: adição de dependências (User e Post) ao módulo principal

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,8 +4,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 
+import { UserModule } from './user/user.module';
+import { PostModule } from './post/post.module';
+
 @Module({
-  imports: [AuthModule] /* Importação de outros módulos */,
+  imports: [
+    AuthModule,
+    UserModule,
+    PostModule,
+  ] /* Importação de outros módulos */,
   controllers: [AppController] /* Controllers pertencentes a este módulo */,
   providers: [AppService] /* Providers (serviços) usados neste módulo */,
   exports: [] /* Torna estes serviços visíveis para outros módulos */,
