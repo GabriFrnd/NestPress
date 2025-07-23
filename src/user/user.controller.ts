37e7cbca63f660ba662cfe33ade51914e6233305
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-22 23:46:40 -0300

    🟢 Feat: implementação de parâmetros dinâmicos no controller com @Param

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,10 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  @Get(':id')
+  /* '@Param()': extrai parâmetros da rota (nesse caso, 'id') */
+  findOne(@Param('id') id: string) {
+    return `Olá, mundo! ("user.controller", ${id})`;
+  }
+}
