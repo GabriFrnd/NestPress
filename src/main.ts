169bc71ce29cb0f4c0043fb6ede2a72d06d3e68f
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-23 00:30:35 -0300

    🟢 Feat: aplicação de ValidationPipe globalmente para validar/transformar dados das requisições

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  /* Validação global de dados, em todas as rotas, recebidos em requisições */
+  app.useGlobalPipes(
+    new ValidationPipe({
+      /* Remove chaves que não tem decoração do DTO */
+      whitelist: true,
+
+      /* Lança um erro caso o usuário mande uma chave que não deveria estar em um determinado local */
+      forbidNonWhitelisted: true,
+
+      /* Tenta transformar um dado */
+      transform: true,
+    }),
+  );
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
