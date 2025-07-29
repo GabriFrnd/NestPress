6d783d667a710e168750e76361f80bf37fe87699
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-29 08:26:57 -0300

    🟢 Feat: adição de pipe customizado para validação de parâmetros no controller do usuário

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
   @Get(':id')
   /* '@Param()': extrai parâmetros da rota (nesse caso, 'id') */
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', CustomParseIntPipe) id: number) {
     return `Olá, mundo! ("user.controller", ${id})`;
   }
 }
