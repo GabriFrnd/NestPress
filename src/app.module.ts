836c441ee48ac983dfed04c2ef1f94c1d41ed708
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-29 08:58:58 -0300

    🟢 Feat: importação e configuração de config module como global

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,11 +7,16 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 
+import { ConfigModule } from '@nestjs/config';
+
 @Module({
   imports: [
     AuthModule,
     UserModule,
     PostModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
   ] /* Importação de outros módulos */,
   controllers: [AppController] /* Controllers pertencentes a este módulo */,
   providers: [AppService] /* Providers (serviços) usados neste módulo */,
