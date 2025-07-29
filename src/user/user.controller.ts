75ad405d90d303a9a3153dd2da917350ba6d4337
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-29 09:01:10 -0300

    🟢 Feat: adição de config service ao user controller

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,8 +1,11 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { CustomParseIntPipe } from 'src/common/pipes/custom-parse-int-pipe.pipe';
 
 @Controller('user')
 export class UserController {
+  constructor(private readonly configService: ConfigService) {}
+
   @Get(':id')
   /* '@Param()': extrai parâmetros da rota (nesse caso, 'id') */
   findOne(@Param('id', CustomParseIntPipe) id: number) {
