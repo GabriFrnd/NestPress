6ec0a706a734344c52e0cab9b45669cf3d521988
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-30 08:20:35 -0300

    🟢 Feat: adição de suporte a SQLite e configuração dinâmica do TypeORM baseada em variáveis de ambiente

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,7 @@ import { UserModule } from './user/user.module';
 import { PostModule } from './post/post.module';
 
 import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
@@ -16,6 +17,31 @@ import { ConfigModule } from '@nestjs/config';
     PostModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      useFactory: () => {
+        if (process.env.DB_TYPE === 'better-sqlite3') {
+          /* Configurações para SQLite3 */
+          return {
+            type: 'better-sqlite3',
+            database: process.env.DB_DATABASE || './db.sqlite',
+            synchronize: process.env.DB_SYNCHRONIZE === '1',
+            autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+          };
+        }
+
+        /* Configurações para PostgreSQL */
+        return {
+          type: 'postgres',
+          host: process.env.DB_HOST,
+          port: parseInt(process.env.DB_PORT || '5432', 10),
+          username: process.env.DB_USERNAME,
+          password: process.env.DB_PASSWORD,
+          database: process.env.DB_DATABASE,
+          synchronize: process.env.DB_SYNCHRONIZE === '1',
+          autoLoadEntities: process.env.DB_AUTO_LOAD_ENTITIES === '1',
+        };
+      },
     }),
   ] /* Importação de outros módulos */,
   controllers: [AppController] /* Controllers pertencentes a este módulo */,
