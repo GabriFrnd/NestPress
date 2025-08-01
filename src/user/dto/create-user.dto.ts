87c45d3609d866a189c268eaf8c5c716a65ecb93
Gabriel Fernandes Feitosa <gabri.frnd21@gmail.com>
2025-07-31 22:13:27 -0300

    🟢 Feat: adição de DTO para criação de usuários com validações

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,14 @@
-export class CreateUserDto {}
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateUserDto {
+  @IsString({ message: `O nome precisa ser do tipo 'string'.` })
+  @IsNotEmpty({ message: `O campo 'nome' não pode estar vazio.` })
+  name: string;
+
+  @IsEmail({}, { message: 'E-mail inválido.' })
+  email: string;
+
+  @IsString({ message: `A senha precisa ser do tipo 'string'.` })
+  @IsNotEmpty({ message: `O campo 'senha' não pode estar vazio.` })
+  password: string;
+}
